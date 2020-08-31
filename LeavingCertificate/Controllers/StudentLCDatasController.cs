690bd446094bd0b0bdc5e259cf2c3c2f81827959
Akshay Dikare <69152213+akshaydikare@users.noreply.github.com>
2020-08-31 17:13:05 +0530

    Add project files.

---
+++ LeavingCertificate/Controllers/StudentLCDatasController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using LeavingCertificate.Models;
+using Rotativa;
+
+namespace LeavingCertificate.Controllers
+{
+    public class StudentLCDatasController : Controller
+    {
+        private LcDataEntities db = new LcDataEntities();
+
+        // GET: StudentLCDatas
+        public ActionResult Index()
+        {
+            return View(db.StudentLCData.ToList());
+        }
+
+        // GET: StudentLCDatas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentLCData studentLCData = db.StudentLCData.Find(id);
+            if (studentLCData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentLCData);
+        }
+
+        // GET: StudentLCDatas/Create
+        public ActionResult Create()
+        {
+            List<string> CountryList = new List<string>();
+            CultureInfo[] CInfoList = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            foreach (CultureInfo CInfo in CInfoList)
+            {
+                RegionInfo R = new RegionInfo(CInfo.LCID);
+                if (!(CountryList.Contains(R.EnglishName)))
+                {
+                    CountryList.Add(R.EnglishName);
+                }
+            }
+
+            CountryList.Sort();
+            ViewBag.CountryList = CountryList;
+            return View();
+        }
+
+        // POST: StudentLCDatas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,AdmissionNo,FullName,Gender,Nationality,MotherTongue,Religion,Caste,SubCaste,BirthPlace,DOBInDigits,DOBInWords,PrevSchlName,AdmittedInClass,AdmissionDate,Progress,ConductBehaviour,DateOfSchlLeaving,ClassFromLeft,ReasonForLeaving,CertificateRcvdDate,ReceivedByName")] StudentLCData studentLCData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentLCData.Add(studentLCData);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentLCData);
+        }
+
+        // GET: StudentLCDatas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentLCData studentLCData = db.StudentLCData.Find(id);
+            if (studentLCData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentLCData);
+        }
+
+        // POST: StudentLCDatas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,AdmissionNo,FullName,Gender,Nationality,MotherTongue,Religion,Caste,SubCaste,BirthPlace,DOBInDigits,DOBInWords,PrevSchlName,AdmittedInClass,AdmissionDate,Progress,ConductBehaviour,DateOfSchlLeaving,ClassFromLeft,ReasonForLeaving,CertificateRcvdDate,ReceivedByName")] StudentLCData studentLCData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentLCData).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentLCData);
+        }
+
+        // GET: StudentLCDatas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentLCData studentLCData = db.StudentLCData.Find(id);
+            if (studentLCData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentLCData);
+        }
+
+        // POST: StudentLCDatas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentLCData studentLCData = db.StudentLCData.Find(id);
+            db.StudentLCData.Remove(studentLCData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Change(string langabb)
+        {
+            if (langabb != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(langabb);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(langabb);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = langabb;
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult PrintById(int? id)
+        {
+            var student = db.StudentLCData.Where(e => e.Id == id).FirstOrDefault();
+            return View(student);
+        }
+        public ActionResult PrintCertificate(int id)
+        {
+            var report = new ActionAsPdf("PrintById", new { id = id })
+            {
+
+                PageMargins = { Left = 5, Bottom = 5, Right = 5, Top = 5 },
+                PageSize = Rotativa.Options.Size.A5
+
+
+            };
+            return report;
+        }
+        public ActionResult Print(int id)
+        {
+            var report = new ActionAsPdf("PrintById", new { id = id });
+            return report;
+        }
+    }
+}
---
+++ LeavingCertificate/Models/StudentLc.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using LeavingCertificate.Models.Resources;
+
+namespace LeavingCertificate.Models
+{
+
+    [MetadataType(typeof(StudentLCMetaData))]
+    public partial class StudentLCData
+    {
+    }
+
+
+    public class StudentLCMetaData
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "AdmissionNumber", ResourceType = typeof(SchoolLc))]
+        public int AdmissionNo { get; set; }
+
+        [Required]
+        [Display(Name = "FullName", ResourceType = typeof(SchoolLc))]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Nationality", ResourceType = typeof(SchoolLc))]
+        public string Nationality { get; set; }
+
+        [Required]
+        [Display(Name = "MotherTongue", ResourceType = typeof(SchoolLc))]
+        public string MotherTongue { get; set; }
+
+        [Required]
+        [Display(Name = "Religion", ResourceType = typeof(SchoolLc))]
+        public string Religion { get; set; }
+
+        [Required]
+        [Display(Name = "Caste", ResourceType = typeof(SchoolLc))]
+        public string Caste { get; set; }
+
+        [Display(Name = "SubCaste", ResourceType = typeof(SchoolLc))]
+        public string SubCaste { get; set; }
+
+        [Required]
+        [Display(Name = "BirthPlace", ResourceType = typeof(SchoolLc))]
+        public string BirthPlace { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "DOBInDigits", ResourceType = typeof(SchoolLc))]
+        public DateTime? DOBInDigits { get; set; }
+
+        [Display(Name = "DOBInWords", ResourceType = typeof(SchoolLc))]
+        public string DOBInWords { get; set; }
+
+        [Display(Name = "PrevSchlName", ResourceType = typeof(SchoolLc))]
+        public string PrevSchlName { get; set; }
+
+        [Display(Name = "AdmittedInClass", ResourceType = typeof(SchoolLc))]
+        public string AdmittedInClass { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "AdmissionDate", ResourceType = typeof(SchoolLc))]
+        public string AdmissionDate { get; set; }
+
+        [Required]
+        [Display(Name = "Progress", ResourceType = typeof(SchoolLc))]
+        public string Progress { get; set; }
+
+        [Display(Name = "ConductBehaviour", ResourceType = typeof(SchoolLc))]
+        public string ConductBehaviour { get; set; }
+
+        [Required]
+        [Display(Name = "DateOfSchlLeaving", ResourceType = typeof(SchoolLc))]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfSchlLeaving { get; set; }
+
+        [Required]
+        [Display(Name = "ClassFromLeft", ResourceType = typeof(SchoolLc))]
+        public string ClassFromLeft { get; set; }
+
+        [Required]
+        [Display(Name = "ReasonForLeaving", ResourceType = typeof(SchoolLc))]
+        public string ReasonForLeaving { get; set; }
+
+        [Display(Name = "CertificateRcvdDate", ResourceType = typeof(SchoolLc))]
+        [DataType(DataType.Date)]
+        public DateTime? CertificateRcvdDate { get; set; }
+
+        [Display(Name = "ReceivedByName", ResourceType = typeof(SchoolLc))]
+        public string ReceivedByName { get; set; }
+
+
+        [Display(Name = "Gender", ResourceType = typeof(SchoolLc))]
+        public string Gender { get; set; }
+
+    }
+}
